9d59ea0b8acc2ef06c49cd6198b58efab6523746
Rahul Kumar <rahul.kumar1099@gmail.com>
2023-07-31 14:55:54 +0530

    Adding .gitignore

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pymysql.cursors
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# MySQL connection details
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DB = os.getenv('MYSQL_DB')
+
+# Connect to the database
+connection = pymysql.connect(host=MYSQL_HOST,
+                             user=MYSQL_USER,
+                             password=MYSQL_PASSWORD,
+                             db=MYSQL_DB,
+                             cursorclass=pymysql.cursors.DictCursor)
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/api/<int:id>')
+def get_student(id):
+    try:
+        with connection.cursor() as cursor:
+            # Query the database
+            sql = "SELECT * FROM Students WHERE Id = %s"
+            cursor.execute(sql, (id,))
+            result = cursor.fetchone()
+
+            if result:
+                return jsonify(result), 200
+            else:
+                return jsonify({'error': 'Student not found'}), 404
+    except Exception as e:
+        return jsonify({'error': 'Database error'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
